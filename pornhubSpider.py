308dee4f04c7a97480106e1f0c67d7948fd6d727
ZhaiCanZC <z840202741@hotmail.com>
2020-03-09 13:16:59 +0000

    fix of getting url

--- pornhubSpider.py
+++ pornhubSpider.py
@@ -2,7 +2,34 @@ import requests
 import json
 import os
 import re
-from bs4 import BeautifulSoup as soup 
+from bs4 import BeautifulSoup as soup
+
+class PornHubVideo(object):
+    def __init__(self, flvars):
+        self.pageUrl = flvars['link_url']
+        self.viewkey = re.search('viewkey=([a-zA-Z0-9]+)',self.pageUrl).group(1)
+        self.title = flvars['video_title']
+        self.imgUrl = flvars['image_url']
+        time = int(flvars['video_duration'])
+        m, s = divmod(time,60)
+        h, m = divmod(m,60)
+        self.duration = '%02d:%02d:%02d' % (h,m,s)
+        self.definitions = []
+        for defin in flvars['mediaDefinitions']:
+            info = {}
+            info['format'] = defin['format']
+            info['quality'] = defin['quality']
+            info['videoUrl'] = defin['videoUrl']
+            self.definitions.append(info)
+        self.definitions.remove(self.definitions[-1])
+    
+    def __str__(self):
+        info = {}
+        for k in self.__dict__.keys():
+            info[k] = getattr(self,k)
+        
+        return json.dumps(info,ensure_ascii=False,indent=4)
+
 link = 'https://www.pornhub.com'
 #获取某分类下任意页数的视频列表
 def getVideoList(types,page='1',search = False):
@@ -10,7 +37,7 @@ def getVideoList(types,page='1',search = False):
     result = []
     url = link+types+str(page)
     rep = requests.get(url)
-    videoList = soup(rep.content)
+    videoList = soup(rep.content,'lxml')
     if search:
         videoList = videoList.find('ul',id = 'videoSearchResult').findAll('li')
     else:
@@ -29,36 +56,39 @@ def getVideoList(types,page='1',search = False):
             continue
             
     return result
-#获取视频全部清晰度,地址,视频编号
+    
+#旧版函数，已停用
 def getVideoUrl(viewkey):
+    raise Exception('This method(getVideoUrl) has been abandoned.Please use "getVideoInfo" instead.')
+    
+#获取视频全部清晰度,地址,视频编号
+def getVideoInfo(viewkey):
     url = link + '/view_video.php?viewkey='+viewkey
     rep = requests.get(url)
-    t = re.findall(r'"mediaDefinitions":\[(.+?)\]',rep.text)[0]
-    try:
-        videoList = json.loads('['+t+']')
-    except:
-        videoList = json.loads('['+t+']}]')
-    videoURL = []
-    t=0
-    for i in videoList:
-        if (i["format"].upper() != 'MP4') or i['videoUrl'] == "":
-            continue
-        try:
-            if int(i["quality"])>t:
-                t=int(i["quality"])
-                videoURL = [i,]
-        except Exception as e:
-            print(e)
-            continue
-    ids = os.path.split(os.path.split(videoURL[0].get('videoUrl'))[0])[1]
-    return [videoURL,ids]
+    doc = soup(rep.content,'lxml')
+    script = doc.find(id='player').script.contents[0]
+    script = re.sub('loadScriptUniqueId[\s\S]+','',script)
+    script += re.search('flashvars[^ ]+',script).group(0) + ';'
+    #print(script)
+    import js2py
+    import ast
+    json_str = str(js2py.eval_js(script))
+    flashvars = ast.literal_eval(json_str)
+    del flashvars['hotspots']
+    
+    video = PornHubVideo(flashvars)
+    #print(flashvars)
+    #print(flashvars['mediaDefinitions'])
+    
+    return video
+    
     
 #根据视频编号获取全部相似推荐
 def getVideoSimilar(ids,page=1):
     url = '%s/video/relateds?ajax=1&id=%s&page=%s&num_per_page=10'%(link,ids,page)
     result=[]
     rep = requests.get(url)
-    videoList = soup(rep.content)
+    videoList = soup(rep.content,'lxml')
     videoList = videoList.findAll('li')
     for i in videoList:
         result.append({
