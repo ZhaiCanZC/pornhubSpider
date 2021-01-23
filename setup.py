58aa410c04ca3acca3dcc78719917eeabe725b03
zoink <zoinkcn@outlook.com>
2021-01-23 23:14:38 +0800

    V 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pornhubSpider",
-    version="1.0.0",
+    version="1.1.0",
     author="ZoinkCN",
     author_email="zoinkcn@outlook.com",
     description="A simple spider of Pornhub",
